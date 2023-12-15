02e1b7b61999a540219421d79ec582e3898e5c69
Divya Sharma <dsrsaknp@gmail.com>
2023-12-15 19:41:40 +0530

    uncommented

--- coding_playground/1-coding_oop_js/day_4.js
+++ coding_playground/1-coding_oop_js/day_4.js
@@ -1,51 +1,51 @@
-// // Stopwatch
+// Stopwatch
 
-// function Stopwatch() {
-//   let start = false;
-//   let stop = false;
-//   let duration = 0;
-//   let startTime = 0;
+function Stopwatch() {
+  let start = false;
+  let stop = false;
+  let duration = 0;
+  let startTime = 0;
 
-//   this.start = function () {
-//     if (!start && !stop) {
-//       start = true;
-//       startTime = new Date();
-//       console.log("started!");
-//     } else {
-//       throw new Error("Stopwatch already running!");
-//     }
-//   };
+  this.start = function () {
+    if (!start && !stop) {
+      start = true;
+      startTime = new Date();
+      console.log("started!");
+    } else {
+      throw new Error("Stopwatch already running!");
+    }
+  };
 
-//   this.stop = function () {
-//     if (!stop && start) {
-//       endTime = new Date();
-//       duration = endTime - startTime;
-//       console.log("stopped!");
-//     } else if (!start) {
-//       throw new Error("Stopwatch not started!");
-//     } else {
-//       throw new Error("Stopwatch already stopped!");
-//     }
-//   };
+  this.stop = function () {
+    if (!stop && start) {
+      endTime = new Date();
+      duration = endTime - startTime;
+      console.log("stopped!");
+    } else if (!start) {
+      throw new Error("Stopwatch not started!");
+    } else {
+      throw new Error("Stopwatch already stopped!");
+    }
+  };
 
-//   this.duration = function () {
-//     console.log(duration);
-//   };
+  this.duration = function () {
+    console.log(duration);
+  };
 
-//   this.reset = function () {
-//     start = false;
-//     stop = false;
-//     duration = 0;
-//     startTime = 0;
-//     console.log("Stopwatch has been reset.");
-//   };
-// }
+  this.reset = function () {
+    start = false;
+    stop = false;
+    duration = 0;
+    startTime = 0;
+    console.log("Stopwatch has been reset.");
+  };
+}
 
-// let timer1 = new Stopwatch();
-// timer1.start();
-// timer1.stop();
-// timer1.duration();
-// timer1.reset;
+let timer1 = new Stopwatch();
+timer1.start();
+timer1.stop();
+timer1.duration();
+timer1.reset;
 
 // #2
 function Stopwatch2() {
