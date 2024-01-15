e662ca03292423b3260de3884680f29a18991d74
Divya Sharma <dsrsaknp@gmail.com>
2024-01-15 11:34:38 +0530

    added currying codes

---
+++ coding_playground/4-coding_js/3-js_questions/question3.js
@@ -0,0 +1,128 @@
+// // Counter function with increment and getValue functionality
+// // (Closure question)
+
+// // #1
+// function privateCounter() {
+//   let count = 0;
+//   return {
+//     increment: function (val = 1) {
+//       count += val;
+//     },
+//     getvalue: function () {
+//       return count;
+//     },
+//   };
+// }
+
+// const counter = privateCounter();
+// console.log(counter.getvalue());
+// counter.increment();
+// console.log(counter.getvalue());
+
+// // console.dir(counter.getvalue);
+
+// // #2
+// function privateSecret() {
+//   let secret = 234243;
+//   return () => secret;
+// }
+
+// const getSecret = privateSecret();
+// console.log(getSecret());
+
+// // #4** - function that multiplies a and b and achieves multiply(a)(b)
+// function multiply(a) {
+//   return function (b) {
+//     return a * b;
+//   };
+// }
+// const product = multiply(10)(4);
+// console.log(product);
+
+// // #4.1**
+// const multiply1 = (num1) => (num2) => num1 * num2;
+// console.log(multiply1(12)(2));
+
+// // #4.2 ***
+// const curry = function (fn) {
+//   let arity = fn.length;
+//   return function f1(...args) {
+//     console.log("args inside f1: ", args);
+//     console.log("arity inside f1: ", arity);
+//     if (args.length >= arity) {
+//       return fn(...args);
+//     } else {
+//       console.log("f1 else part");
+//       return function f2(...nextArg) {
+//         let newArgs = args.concat(nextArg);
+//         console.log("args inside f2: ", newArgs);
+//         console.log("arity inside f2: ", arity);
+//         return f1(...newArgs);
+//       };
+//     }
+//   };
+// };
+
+// const curriedSum = curry((a, b, c) => a + b + c);
+// console.log("partially executed ", curriedSum(1));
+// console.log("partially executed ", curriedSum(1)(2));
+// console.log(curriedSum(1)(2, 3));
+// console.log(curriedSum(1)(2, 3)(4));
+
+// // #4.3***
+// const curry = function (fn) {
+//   let arity = fn.length;
+//   return function f1(...args) {
+//     console.log("args inside f1: ", args);
+//     console.log("arity inside f1: ", arity);
+//     if (args.length >= arity) {
+//       return fn(...args);
+//     } else {
+//       console.log("f1 else part");
+//       return function f2(...nextArg) {
+//         let newArgs = args.concat(nextArg);
+//         console.log("args inside f2: ", newArgs);
+//         console.log("arity inside f2: ", arity);
+//         return f1(...newArgs);
+//       };
+//     }
+//   };
+// };
+
+// const get = curry((property, object) => object[property]); // f1
+// console.log("get", get);
+
+// const getId = get("id"); // f2
+// console.log("getId", getId);
+
+// console.log(getId({ id: 15 })); // finally passed all arguments
+
+// #4.4***
+// 1. on first call, returns f1
+// 2. if incomplete parameters, returns f2
+// 3. incomplete parameters, repeat 2.
+// 4. complete parameters, then execute fn.
+const curry = function (fn) {
+  let arity = fn.length;
+  return function f1(...args) {
+    // console.log("args inside f1: ", args);
+    // console.log("arity inside f1: ", arity);
+    if (args.length >= arity) {
+      return fn(...args);
+    } else {
+      // console.log("f1 else part");
+      return function f2(...nextArg) {
+        let newArgs = args.concat(nextArg);
+        // console.log("args inside f2: ", newArgs);
+        // console.log("arity inside f2: ", arity);
+        return f1(...newArgs);
+      };
+    }
+  };
+};
+
+const get = curry((property, object) => object[property]);
+const getId = get("id");
+const map = curry((fn, values) => values.map(fn));
+const getIds = map(getId);
+console.log(getIds([{ id: 23 }]));
