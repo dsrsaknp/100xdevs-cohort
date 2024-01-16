6dc02732731a456a773f08562168fc4beeae27f0
Divya Sharma <dsrsaknp@gmail.com>
2024-01-16 11:42:09 +0530

    shuffle

--- coding_playground/4-coding_js/3-js_questions/day5.js
+++ coding_playground/4-coding_js/3-js_questions/day5.js
@@ -8,9 +8,20 @@
 // };
 // range(10, 20);
 
-// #1.1 - Avoid loop
-const range11 = function (start = 1, end = 1) {
-  return [...Array(end - start).keys()].map((el) => el + start);
+// // #1.1 - Avoid loop
+// const range11 = function (start = 1, end = 1) {
+//   return [...Array(end - start).keys()].map((el) => el + start);
+// };
+// console.log(range11(10, 20));
+// console.log(range11(1, 20));
+
+// #2 - shuffle function
+const shuffle = (items) => {
+  // items could be elements, objects, array, array of arrays
+  return items
+    .map((item) => ({ sort: Math.random(), value: item }))
+    .sort((item1, item2) => item1.sort - item2.sort)
+    .map((item) => item.value);
 };
-console.log(range11(10, 20));
-console.log(range11(1, 20));
+
+console.log(shuffle([2, 1, 3, 4]));
