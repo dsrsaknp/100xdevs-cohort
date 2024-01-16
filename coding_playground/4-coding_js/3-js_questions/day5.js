b4be98d32f38a294bf110f556889a654abf6da1e
Divya Sharma <dsrsaknp@gmail.com>
2024-01-16 17:15:24 +0530

    es6 classes implementation using  Javascript prototypes

--- coding_playground/4-coding_js/3-js_questions/day5.js
+++ coding_playground/4-coding_js/3-js_questions/day5.js
@@ -1,52 +1,52 @@
-// // #1 - implement range function
-// const range = function (start = 1, end = 0) {
-//   const result = [];
-//   for (let i = start; i <= end; i++) {
-//     result.push(i);
-//   }
-//   return result;
-// };
-// range(10, 20);
+// #1 - implement range function
+const range = function (start = 1, end = 0) {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+};
+range(10, 20);
 
-// // #1.1 - Avoid loop
-// const range11 = function (start = 1, end = 1) {
-//   return [...Array(end - start).keys()].map((el) => el + start);
-// };
-// console.log(range11(10, 20));
-// console.log(range11(1, 20));
+// #1.1 - Avoid loop
+const range11 = function (start = 1, end = 1) {
+  return [...Array(end - start).keys()].map((el) => el + start);
+};
+console.log(range11(10, 20));
+console.log(range11(1, 20));
 
-// // #2 - shuffle function
-// const shuffle = (items) => {
-//   // items could be elements, objects, array, array of arrays
-//   return items
-//     .map((item) => ({ sort: Math.random(), value: item }))
-//     .sort((item1, item2) => item1.sort - item2.sort)
-//     .map((item) => item.value);
-// };
+// #2 - shuffle function
+const shuffle = (items) => {
+  // items could be elements, objects, array, array of arrays
+  return items
+    .map((item) => ({ sort: Math.random(), value: item }))
+    .sort((item1, item2) => item1.sort - item2.sort)
+    .map((item) => item.value);
+};
 
-// console.log(shuffle([2, 1, 3, 4]));
+console.log(shuffle([2, 1, 3, 4]));
 
-// // #3 - Number of occurrances of minimum values in list
-// const minValueFrequency = (items) => {
-//   let f = items
-//     .sort((item1, item2) => item1 - item2)
-//     .reduce((acc, item) => {
-//       if (items[0] === item) acc += 1;
-//       return acc;
-//     }, 0);
-//   return f;
-// };
+// #3 - Number of occurrances of minimum values in list
+const minValueFrequency = (items) => {
+  let f = items
+    .sort((item1, item2) => item1 - item2)
+    .reduce((acc, item) => {
+      if (items[0] === item) acc += 1;
+      return acc;
+    }, 0);
+  return f;
+};
 
-// console.log(minValueFrequency([4, 5, 5, 6, 3, 2, 3, 5, 4, 3, 2, 2, 2]));
+console.log(minValueFrequency([4, 5, 5, 6, 3, 2, 3, 5, 4, 3, 2, 2, 2]));
 
-// // #3.1 - optimum method
-// const minValueOccurances = (items) => {
-//   const minValue = Math.min(...items);
-//   const minValueArray = items.filter((item) => item === minValue);
-//   return minValueArray.length;
-// };
+// #3.1 - optimum method
+const minValueOccurances = (items) => {
+  const minValue = Math.min(...items);
+  const minValueArray = items.filter((item) => item === minValue);
+  return minValueArray.length;
+};
 
-// console.log(minValueFrequency([4, 5, 5, 6, 3, 2, 3, 5, 4, 3, 2, 2, 2]));
+console.log(minValueFrequency([4, 5, 5, 6, 3, 2, 3, 5, 4, 3, 2, 2, 2]));
 
 // #4 - This
 
@@ -151,3 +151,96 @@ class Item7 {
 }
 const item7 = new Item7();
 item7.getItem();
+
+// #6 - Design a class employee with id andname given at construction, and salary as property. Create Manager class wh is an employee and department property
+class Employee {
+  constructor(id, name) {
+    if (!id || !name) throw new Error("Employee id and name are mandatory.");
+    this.id = id;
+    this.name = name;
+  }
+
+  setSalary(s) {
+    this.salary = s;
+  }
+  getSalary() {
+    return this.salary;
+  }
+  getId() {
+    return this.id;
+  }
+  getName() {
+    return this.name;
+  }
+}
+
+class Manager extends Employee {
+  setDepartment(name) {
+    this.name = name;
+  }
+  getDepartment() {
+    return this.department;
+  }
+}
+
+const emp1 = new Employee(1, 34);
+emp1.salary = "160k$";
+
+console.log(emp1.getSalary());
+console.log(emp1.getId());
+console.log(emp1.getName());
+
+const m = new Manager(12, "Jonathan");
+console.log(m);
+m.setDepartment = "Research";
+console.log(m);
+
+// #6.1 - Design the same classes using Javascript prototypes
+function Employee1(id, name) {
+  if (!id || !name) throw new Error("Employee1 id and name are mandatory.");
+  this.id = id;
+  this.name = name;
+}
+
+Employee1.prototype.setSalary = function (salary) {
+  this.salary = salary;
+};
+
+Employee1.prototype.getSalary = function () {
+  return this.salary;
+};
+
+Employee1.prototype.getId = function () {
+  return this.id;
+};
+Employee1.prototype.getName = function () {
+  return this.name;
+};
+
+function Manager1(id, name, department) {
+  Employee1.call(this, id, name);
+  this.department = department;
+}
+
+Manager1.prototype = Object.create(Employee1.prototype);
+Manager1.prototype.constructor = Manager1;
+
+Manager1.prototype.setDepartment = function (name) {
+  this.department = name;
+};
+
+Manager1.prototype.getDepartment = function () {
+  return this.department;
+};
+
+const emp2 = new Employee1(21, "Maximus John");
+emp2.setSalary("230k$");
+emp2.getSalary();
+console.log(emp2);
+
+const m2 = new Manager1(23, "Howard Tenner");
+m2.setDepartment("Intelligence");
+m2.getDepartment();
+console.log(m2);
+
+
